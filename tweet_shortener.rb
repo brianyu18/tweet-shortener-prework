9d0a098daff4d2b56c80bc64ca2c966d68ed1578
brianyu18 <brianyu1990@gmail.com>
2018-09-28 08:14:50 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,6 +40,29 @@ def word_substituter(tweet)
   end
   return arr.join (" ")
 end
+
+def bulk_tweet_shortener(array_tweets)
+  array_tweets.each do |check|
+    word_substituter(check)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    new_tweet = tweet[0..140] + "..."
+  else
+    tweet
+  end
+end
+  
     
   nuarr.join (" ")
 # Write your code here.
